fd71d7d8f328d31d4ee0c78230fb196c017b524f
l3r0ux <lerouxvermeulen@gmail.com>
2021-06-02 15:14:19 +0200

    more base setup and add api key to config file

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('script connected')
+async function getMovie() {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: config.API_KEY,
+            s: "The Avengers"
+        }
+    })
+    console.log(response.data);
+    for (let movie of response.data.Search) {
+        console.log(movie.Title)
+    }
+}
+
+getMovie();
