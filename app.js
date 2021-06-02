dc4d952361abcdefb00bdcc7e6d5b3b42e11c9ed
l3r0ux <lerouxvermeulen@gmail.com>
2021-06-02 16:57:10 +0200

    add basic layout of project

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-async function getMovie() {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: config.API_KEY,
-            s: "The Avengers"
-        }
-    })
-    console.log(response.data);
-    for (let movie of response.data.Search) {
-        console.log(movie.Title)
-    }
-}
+// async function getMovie() {
+//     const response = await axios.get('http://www.omdbapi.com/', {
+//         params: {
+//             apikey: config.API_KEY,
+//             s: "The Avengers"
+//         }
+//     })
+//     console.log(response.data);
+//     for (let movie of response.data.Search) {
+//         console.log(movie.Title)
+//     }
+// }
 
-getMovie();
+// getMovie();
